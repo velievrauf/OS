2b3b33d3d64fd0a5eab3ec161ea99886cec85a6a
velievrauf <145988152+velievrauf@users.noreply.github.com>
2024-12-07 00:12:46 +0300

    Update lab3.c

--- lab_3/lab3.c
+++ lab_3/lab3.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/wait.h>
-#include <sys/mman.h>
-#include <string.h>
 
-#define SHARED_MEMORY_SIZE 1024
+#define SHARED_MEMORY_NAME "/my_shm"
+#define SHARED_MEMORY_SIZE sizeof(struct SharedMemory)
 
 struct SharedMemory {
     float numbers[100];
     int count;
-    volatile int flag;  
-    char result[256];    
+    volatile int flag; 
+    char result[256];   
 };
 
 int main() {
-    int shm_fd = shm_open("/my_shm", O_CREAT | O_RDWR, 0666);
+    int shm_fd = shm_open(SHARED_MEMORY_NAME, O_CREAT | O_RDWR, 0666);
     if (shm_fd == -1) {
         perror("shm_open");
         exit(EXIT_FAILURE);
     }
     
-    if (ftruncate(shm_fd, sizeof(struct SharedMemory)) == -1) {
+    if (ftruncate(shm_fd, SHARED_MEMORY_SIZE) == -1) {
         perror("ftruncate");
         exit(EXIT_FAILURE);
     }
 
-    struct SharedMemory *shm_ptr = mmap(NULL, sizeof(struct SharedMemory), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    struct SharedMemory *shm_ptr = mmap(NULL, SHARED_MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shm_ptr == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
@@ -41,34 +42,18 @@ int main() {
     }
 
     if (pid == 0) {  
-        close(shm_fd); 
+        close(shm_fd);  
 
-        while (1) {
-            if (shm_ptr->flag == 1) {  
-                float result = shm_ptr->numbers[0];
-                for (int i = 1; i < shm_ptr->count; i++) {
-                    if (shm_ptr->numbers[i] == 0) {
-                        snprintf(shm_ptr->result, sizeof(shm_ptr->result), "Ошибка: деление на ноль");
-                        shm_ptr->flag = 2;
-                        return 0;
-                    }
-                    result /= shm_ptr->numbers[i];
-                }
-
-                snprintf(shm_ptr->result, sizeof(shm_ptr->result), "Результат: %.2f", result);
-                shm_ptr->flag = 2;  
-            }
-            usleep(100000);  
-        }
-
-        return 0;
-    } else {  
+        execl("./kid", "kid", NULL);
+        perror("execl");
+        exit(EXIT_FAILURE);
+    } else { 
         close(shm_fd);  
 
         printf("Введите числа (разделенные пробелами), завершите <endline>: ");
-
         float numbers[100];
         int count = 0;
+        
         while (scanf("%f", &numbers[count]) != EOF) {
             count++;
         }
@@ -81,10 +66,10 @@ int main() {
             usleep(100000);  
         }
 
-        printf("%s\n", shm_ptr->result);
+        printf("Результат: %s\n", shm_ptr->result);
 
-        munmap(shm_ptr, sizeof(struct SharedMemory));
-        shm_unlink("/my_shm");
+        munmap(shm_ptr, SHARED_MEMORY_SIZE);
+        shm_unlink(SHARED_MEMORY_NAME);
 
         wait(NULL);
     }
